9632c236e670f34800cfea633f89e6ba8ea8ea08
Nathan Klingensmith <njklingensmith@wpi.edu>
2021-09-14 11:36:29 -0400

    Add sidebar-info template component

---
+++ data/details.js
@@ -0,0 +1,25 @@
+const Details = [
+    {
+        title: 'Address',
+        subtitle: '5 Wachusett St<br>Worcester, MA 01609<br>United States'
+    },
+    {
+        title: 'Phone',
+        subtitle: '(219) 670-9120'
+    },
+    {
+        title: 'Email',
+        href: 'mailto: njklingensmith@wpi.edu',
+        link: 'njklingensmith@wpi.edu'
+    },
+    {
+        title: 'LinkedIn',
+        href: 'https://linkedin.com/in/thenjk',
+        link: 'linkedin.com/in/theNJK'
+    },
+    {
+        title: 'Web Portfolio',
+        href: 'https://thenjk.com',
+        link: 'theNJK.com'
+    }
+];
---
+++ data/skills.js
@@ -0,0 +1,22 @@
+const Skills = [
+    {
+        title: 'HTML, CSS/SCSS',
+        subtitle: '4 years'
+    },
+    {
+        title: 'Javascript/Typescript (NodeJS, VueJS, AngularJS)',
+        subtitle: '3 years'
+    },
+    {
+        title: 'Git (Github, Gitlab)',
+        subtitle: '3 years'
+    },
+    {
+        title: 'Figma, AdobeXD',
+        subtitle: '1 year'
+    },
+    {
+        title: 'Amazon Web Services',
+        subtitle: '1 year'
+    }
+];
