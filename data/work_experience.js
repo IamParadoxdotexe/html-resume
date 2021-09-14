7a2afb35054be3565ed0f46828b896151a1631f8
Nathan Klingensmith <njklingensmith@wpi.edu>
2021-09-14 10:21:22 -0400

    Add mustache template for experience entry

---
+++ data/work_experience.js
@@ -0,0 +1,21 @@
+const WorkExperience = [
+    {
+        title: 'Angular Web Developer',
+        subtitle: 'Bio-CS Bridge Project',
+        tag: '08/2019 - present',
+        content: `<ul>
+            <li>Built a new platform from the ground up for disseminating a network of transdisciplinary lesson plans to high school teachers</li>
+            <li>Overhauled a mobile web app to refine user experience and enhance functionality for 100 monthly users</li>
+            <li>Expanded a data exploration platform to provide dynamic selection, analysis, and visualization of 10,000 data points</li>
+        </ul>`
+    },
+    {
+        title: 'Network Automation Intern',
+        subtitle: 'State Street Corporation',
+        tag: '06/2019 - 08/2019',
+        content: `<ul>
+            <li>Developed and deployed a web portal to automate employee onoboarding onto the Cisco phone network</li>
+            <li>Initiated a department-wide switch from bash scripts to Python automation for increased productivity</li>
+        </ul>`
+    }
+];
